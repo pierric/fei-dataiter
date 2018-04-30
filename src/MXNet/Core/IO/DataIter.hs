796685a12d965cfc4a89a81730bf39a936c5d315
Wu, Jiasen <jiasenwu@kpmg.com>
2018-04-30 17:13:26 +0200

    add a test case

--- src/MXNet/Core/IO/DataIter.hs
+++ src/MXNet/Core/IO/DataIter.hs
@@ -8,22 +8,23 @@ import MXNet.Core.Base.NDArray (NDArray(..))
 import MXNet.Core.Base.Internal
 import qualified MXNet.Core.IO.Internal as I
 
+type StreamData a = Stream (Of (NDArray a, NDArray a)) IO Int
+
 imageRecordIter :: (MatchKVList kvs I.ImageRecordIter_Args, ShowKV kvs, DType a) => 
-                   HMap kvs -> Stream (Of (NDArray a, NDArray a)) IO Int
+                   HMap kvs -> StreamData a
 imageRecordIter args = do
     cnt  <- liftIO (newIORef 0)
     iter <- liftIO (I.imageRecordIter args)
-    let loop = do
-        valid <- liftIO $ do 
-            modifyIORef cnt (+1)
-            checked $ mxDataIterNext iter
-        if valid == 0
-          then liftIO (readIORef cnt)
-          else do
-            item <- liftIO $ do 
-                dat <- checked $ mxDataIterGetData  iter
-                lbl <- checked $ mxDataIterGetLabel iter
-                return (NDArray dat, NDArray lbl)
-            yield item
-            loop
+    let loop = do valid <- liftIO $ do 
+                      modifyIORef cnt (+1)
+                      checked $ mxDataIterNext iter
+                  if valid == 0
+                  then liftIO (readIORef cnt)
+                  else do
+                      item <- liftIO $ do 
+                          dat <- checked $ mxDataIterGetData  iter
+                          lbl <- checked $ mxDataIterGetLabel iter
+                          return (NDArray dat, NDArray lbl)
+                      yield item
+                      loop
     loop
---
+++ test/imagerec.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Test.Hspec
+import Streaming.Prelude
+import MXNet.Core.Base
+
+import MXNet.Core.IO.DataIter
+
+main :: IO ()
+main = hspec $ do
+  describe "ImageRecordIter" $ do
+    it "batch-size = 32" $ do
+        let sr = imageRecordIter (add @"path_imgrec" "test/data/cifar10_val.rec" $ 
+                                  add @"data_shape" [3,28,28] $
+                                  add @"batch_size" 32 nil) :: StreamData Float
+        effects sr `shouldReturn` 314
+    it "batch-size = 128" $ do
+      let sr = imageRecordIter (add @"path_imgrec" "test/data/cifar10_val.rec" $ 
+                                add @"data_shape" [3,28,28] $
+                                add @"batch_size" 128 nil) :: StreamData Float
+      effects sr `shouldReturn` 80
